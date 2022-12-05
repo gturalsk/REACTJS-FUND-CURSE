e0bf612894dc4dc9345b6b1768aaa2e5b688d532
Evgeniy Bazilov <jenya12d@gmail.com>
2022-12-05 07:21:55 +0500

    Start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import "./components/styles/App.css";
 
 function App() {
+  const [value, setValue] = useState("текст в инпуте");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Javackript</strong>
+          <div>Javackript - это язык программирование!</div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
     </div>
   );
 }
